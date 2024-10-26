01a3d01a3093eadd0e05a59a3b395ee87f9b4d9e
Alejandro.Lazaro <Alejandro.Lazaro@jalasoft.com>
2024-10-26 12:13:06 -0400

    [P3] Simple integration test

---
+++ ShoppingCartServiceTest/DataAccess/DatabaseFixture.cs
@@ -0,0 +1,91 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ShoppingCartServiceTest.DataAccess
+{
+    public class DatabaseFixture : IDisposable
+    {
+        public DatabaseFixture()
+        {
+            if (DockerStart())
+            {
+                WaitForMongoDbConnection("mongodb://127.0.0.1:1111", "ShoppingCartDb");
+            }
+        }
+
+        private bool WslCommand(string command)
+        {
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "wsl",
+                    Arguments = command,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                }
+            };
+
+            process.Start();
+
+            string output = process.StandardOutput.ReadToEnd();
+            string error = process.StandardError.ReadToEnd();
+
+            process.WaitForExit();
+
+            if (process.ExitCode != 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool DockerStart()
+        {
+            return WslCommand("docker run -d -p 1111:27017 --name int_test mongo");
+        }
+
+        private bool WaitForMongoDbConnection(string connectionString, string dbName)
+        {
+            var probeTask = Task.Run(() =>
+            {
+                var isAlive = false;
+                var client = new MongoClient(connectionString);
+
+                for (var i = 0; i < 3000; i++)
+                {
+                    client.GetDatabase(dbName);
+                    var server = client.Cluster.Description.Servers.FirstOrDefault();
+                    isAlive = server != null &&
+                             server.HeartbeatException == null &&
+                             server.State == MongoDB.Driver.Core.Servers.ServerState.Connected;
+
+                    if (isAlive)
+                    {
+                        break;
+                    }
+
+                    Thread.Sleep(100);
+                }
+
+                return isAlive;
+            });
+
+            probeTask.Wait();
+
+            return probeTask.Result;
+        }
+
+        public void Dispose()
+        {
+            WslCommand("docker rm -f int_test");
+        }
+    }
+}
---
+++ ShoppingCartServiceTest/DataAccess/SoppingCartRepositoryTest.cs
@@ -0,0 +1,64 @@
+﻿using ShoppingCartService.Config;
+using ShoppingCartService.DataAccess;
+using ShoppingCartService.DataAccess.Entities;
+using ShoppingCartService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace ShoppingCartServiceTest.DataAccess
+{
+    public class SoppingCartRepositoryTest : IClassFixture<DatabaseFixture>
+    {
+        private readonly DatabaseFixture _databaseFixture;
+        private readonly IShoppingCartDatabaseSettings _settings;
+        public SoppingCartRepositoryTest(DatabaseFixture databaseFixture)
+        {
+            _databaseFixture = databaseFixture;
+            _settings = new ShoppingCartDatabaseSettings()
+            {
+                CollectionName = "ShoppingCart",
+                ConnectionString = "mongodb://localhost:1111",
+                DatabaseName = "ShoppingCartDb"
+            };
+        }
+
+        [Fact]
+        public void Create_WhenIsCalled_ThenReturnCartRecord()
+        {
+            //Arrange
+            var repository = new ShoppingCartRepository(_settings);
+
+            var cart = new Cart()
+            {
+                CustomerId = Guid.NewGuid().ToString(),
+                CustomerType = CustomerType.Standard,
+                ShippingMethod = ShippingMethod.Standard,
+                ShippingAddress = new Address()
+                {
+                    Country = "Bolivia",
+                    City = "La Paz",
+                    Street = "Calle 1"
+                },
+                Items = new List<Item>()
+                {
+                    new Item()
+                    {
+                        ProductId = Guid.NewGuid().ToString(),
+                        ProductName = "Test1",
+                        Price = 10,
+                        Quantity = 1,
+                    }
+                }
+            };
+
+            //Act
+            var cartCreated = repository.Create(cart);
+
+            //Assert
+
+            Assert.NotEmpty(cartCreated.Id);
+        }
+    }
+}
